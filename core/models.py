3292486d99d6d94602fc8fe1c9ad505377995a0f
Daniel Martínez <danieljosuemartinezjarquin3@gmail.com>
2026-07-21 14:34:39 -0600

    Add year and color fields to Car model and create corresponding migrations

--- core/models.py
+++ core/models.py
@@ -5,3 +5,8 @@ from django.db import models
 
 class Car(models.Model):
     title = models.TextField(max_length=100)
+    year = models.TextField(max_length=4, null=True)
+    color = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.title}-{self.year}"
