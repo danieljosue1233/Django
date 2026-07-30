c9e748e56a0cff376048ace92b1406538c4b12ba
Daniel Martínez <danieljosuemartinezjarquin3@gmail.com>
2026-07-30 17:49:41 -0600

    Add order management features with views, forms, and admin integration

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderProduct
+
+
+class OrderProductInlineAdmin(admin.TabularInline):
+    model = OrderProduct
+    extra = 0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.inlines = [OrderProductInlineAdmin]
+
+
+admin.site.register(Order, OrderAdmin)
